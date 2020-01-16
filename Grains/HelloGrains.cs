f8ea687e36d28f43581a664e59a8e97875b7ad42
functio19 <15071359483@163.com>
2020-01-16 21:18:18 +0800

    OrleansHelloWorld

---
+++ Grains/HelloGrains.cs
@@ -0,0 +1,27 @@
+﻿using GrainsInterface;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class HelloGrain : Grain, IHelloGrains
+    {
+        private readonly ILogger logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        public Task<string> SayHello(string greeting)
+        {
+            logger.LogInformation($"\n SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
---
+++ GrainsInterface/IHellos.cs
@@ -0,0 +1,14 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrainsInterface
+{
+    public interface IHelloGrains : IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+}
+
